66a5055530cca64573e3c5e5888b072f6c1e1388
vinaya.varghese <varghese.vinu38@gmail.com>
2024-07-27 13:11:17 +0530

    Resolved merge conflicts and removed redundant function

--- helper/cartHelper.js
+++ helper/cartHelper.js
@@ -37,23 +37,23 @@ const getcart = async (userId) => {
     }
 };
 
-        const calculateTotalPricefromItems = (items) => {
-            return items.reduce((total, item) => total + (item.price * item.quantity), 0);
-        };
+    const calculateTotalPricefromItems = (items) => {
+        return items.reduce((total, item) => total + (item.price * item.quantity), 0);
+    };
 
-     const calculateTotalPrice = async (userId) => {
-         try {
-        const cartItems = await cartModel.findOne({ userId }).populate("items.productId");
-        if (cartItems) {
-            const totalPrice = calculateTotalPricefromItems(cartItems.items);
-            return parseFloat(totalPrice);
-        }
+    const calculateTotalPrice = async (userId) => {
+        try {
+    const cartItems = await cartModel.findOne({ userId }).populate("items.productId");
+    if (cartItems) {
+        const totalPrice = calculateTotalPricefromItems(cartItems.items);
+        return parseFloat(totalPrice);
+    }
         return 0;
         } catch (error) {
         // console.error("Error calculating total price:", error);
         throw error;
-        }
-    };
+    }
+};
 
 const updateQuantities = async (userId, productId, action) => {
     try {
